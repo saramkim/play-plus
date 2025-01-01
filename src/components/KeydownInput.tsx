c9a0f88d023cf4a141e34459d1f197c869a66096
saramkim <saramkimm@gmail.com>
2025-01-01 14:45:53 +0900

    feat: add reset button to KeydownInput component

--- src/components/KeydownInput.tsx
+++ src/components/KeydownInput.tsx
@@ -6,19 +6,25 @@ interface KeydownInputProps {
 }
 
 const KeydownInput = ({ value, onChange }: KeydownInputProps) => {
-  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    onChange(e.code);
-  };
-
   return (
-    <input
-      className='input'
-      type='text'
-      value={value}
-      placeholder={getMessage('press_any_key')}
-      onKeyDown={handleKeyDown}
-      readOnly
-    />
+    <div className='relative w-full group'>
+      <input
+        className='input'
+        type='text'
+        value={value}
+        placeholder={getMessage('press_any_key')}
+        onKeyDown={({ code }) => code !== value && onChange(code)}
+        readOnly
+      />
+      {value && (
+        <button
+          className='absolute right-3 top-1/2 -translate-y-1/2 text-gray-500 hidden group-hover:block group-focus-within:block'
+          onClick={() => onChange('')}
+        >
+          ✖
+        </button>
+      )}
+    </div>
   );
 };
 
