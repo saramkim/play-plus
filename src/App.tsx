4ed09bba8ed6d416e5831ef1356d3d5b925d279e
saramkim <saramkimm@gmail.com>
2025-01-01 15:52:41 +0900

    fix: prevent rendering default page before last viewed page is loaded

--- src/App.tsx
+++ src/App.tsx
@@ -12,13 +12,13 @@ const pageList = Object.values(PAGE_NAME);
 const { SAVED_SUBTITLES, SUBTITLE_SETTING, VIDEO_SETTING } = PAGE_NAME;
 
 const pageMap = {
-  [SAVED_SUBTITLES]: () => <SavedSubtitlesPage />,
-  [SUBTITLE_SETTING]: () => <SubtitleSettingPage />,
-  [VIDEO_SETTING]: () => <VideoSettingPage />,
+  [SAVED_SUBTITLES]: <SavedSubtitlesPage />,
+  [SUBTITLE_SETTING]: <SubtitleSettingPage />,
+  [VIDEO_SETTING]: <VideoSettingPage />,
 };
 
 function App() {
-  const [page, setPage] = useState<PageName>(pageList[0]);
+  const [page, setPage] = useState<PageName | null>(null);
 
   useEffect(() => {
     (async () => {
@@ -29,17 +29,19 @@ function App() {
         setPage(pageList[0]);
       }
     })();
-  });
+  }, []);
 
   const navigate = (page: PageName) => {
     setPage(page);
     setLocalStorage('lastViewedPage', page);
   };
 
+  if (!page) return null;
+
   return (
     <div className='h-screen flex flex-col select-none text-nowrap text-[13px]'>
       <Header pageList={pageList} currentPage={page} navigate={navigate} />
-      <main className='h-full overflow-auto p-4'>{pageMap[page]()}</main>
+      <main className='h-full overflow-auto p-4'>{pageMap[page]}</main>
       <Footer />
     </div>
   );
