05a41c8a60c057a71cbac4894412f325e6f59cc1
saramkim <saramkimm@gmail.com>
2025-03-02 16:53:09 +0900

    refactor: change focus ring width to 1px

--- src/ui/components/ui/switch.tsx
+++ src/ui/components/ui/switch.tsx
@@ -8,7 +8,7 @@ function Switch({ className, ...props }: React.ComponentProps<typeof SwitchPrimi
     <SwitchPrimitive.Root
       data-slot='switch'
       className={cn(
-        'peer data-[state=checked]:bg-primary data-[state=unchecked]:bg-input focus-visible:border-ring focus-visible:ring-ring/50 inline-flex h-6 w-11 shrink-0 items-center rounded-full border-2 border-transparent shadow-xs transition-all outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50',
+        'peer data-[state=checked]:bg-primary data-[state=unchecked]:bg-input focus-visible:border-ring focus-visible:ring-ring/50 inline-flex h-6 w-11 shrink-0 items-center rounded-full border-2 border-transparent shadow-xs transition-all outline-none focus-visible:ring-[1px] disabled:cursor-not-allowed disabled:opacity-50',
         className
       )}
       {...props}
