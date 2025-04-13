dfd70477dbd02b665bc9c1b70f76b52d7d339b05
saramkim <saramkimm@gmail.com>
2025-04-14 00:08:06 +0900

    fix: run webpack-bundle-analyzer only in production mode

--- webpack.config.js
+++ webpack.config.js
@@ -62,7 +62,7 @@ module.exports = {
     new CopyWebpackPlugin({
       patterns: [{ from: 'public', to: '.' }],
     }),
-    new BundleAnalyzerPlugin(),
+    ...(isProd ? [new BundleAnalyzerPlugin()] : []),
   ],
   optimization: {
     splitChunks: {
