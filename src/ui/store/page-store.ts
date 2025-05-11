9088d96e1ff6240911c1bd327497b919dfbd0ccf
saramkim <saramkimm@gmail.com>
2025-05-11 20:51:35 +0900

    feat(ui): add page params to store

--- src/ui/store/page-store.ts
+++ src/ui/store/page-store.ts
@@ -2,18 +2,36 @@ import { PAGE_NAME, PageName } from '@utils/constants';
 import { create } from 'zustand';
 import { combine, persist } from 'zustand/middleware';
 
+type PageParams = {
+  [PAGE_NAME.SUBTITLE_SETTING]: never;
+  [PAGE_NAME.VIDEO_SETTING]: never;
+  [PAGE_NAME.REVIEW]: never;
+  [PAGE_NAME.SUBTITLE_ANALYSIS]: never;
+  [PAGE_NAME.SUBTITLE_REGISTRATION]: never;
+};
+
+export type PageStore = ReturnType<typeof usePageStore.getState>;
+
 export const usePageStore = create(
   persist(
     combine(
       {
         currentPage: Object.values(PAGE_NAME)[0],
+        params: {} as Partial<PageParams>,
       },
-      (set) => ({
-        setPage: (page: PageName) => set({ currentPage: page }),
+      (set, get) => ({
+        setPage: <T extends PageName>(page: T, params?: PageParams[T]) =>
+          set({ currentPage: page, params: { [page]: params } }),
+        getParams: (page: PageName) => get().params[page],
       })
     ),
     {
       name: 'page-store',
+      partialize: (state) => ({ currentPage: state.currentPage }),
     }
   )
 );
+
+export const usePageParams = (page: PageName) => {
+  return usePageStore.getState().getParams(page);
+};
